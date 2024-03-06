231795ce77d86aa924bdf936559026795a31bf41
jpgonzalez <jpgonzalez@106-03.info2>
2024-03-06 10:08:39 +0100

    1 commit

---
+++ Tema5.Crud/src/ejercicio01/Alumno.java
@@ -0,0 +1,52 @@
+package ejercicio01;
+
+import java.sql.Time;
+
+public class Alumno {
+
+	private String nombre = "";
+	private double notaMedia = 0;
+
+	public Alumno() {
+
+	}
+
+	public Alumno(String nombre, double notaMedia) {
+		if(nombre != null && !nombre.equals("")) {
+			this.nombre = nombre;
+		}
+		if(notaMedia > 0) {
+			this.notaMedia = notaMedia;
+		}
+	}
+
+	public String getNombre() {
+		return nombre;
+	}
+
+	public void setNombre(String nombre) {
+		if(nombre != null && !nombre.equals("")) {
+			this.nombre = nombre;
+		}
+	}
+
+	public double getNotaMedia() {
+		return notaMedia;
+	}
+
+	public void setNotaMedia(double notaMedia) {
+		if(notaMedia > 0) {
+			this.notaMedia = notaMedia;
+		}
+	}
+	
+	public String toString() {
+		String cadena = "";
+		
+		cadena += "Nombre: " + this.nombre + "\n";
+		cadena += "Nota Media: " + this.notaMedia + "\n";
+	
+		return cadena;
+	}
+
+}
---
+++ Tema5.Crud/src/ejercicio01/Main.java
@@ -0,0 +1,11 @@
+package ejercicio01;
+
+public class Main {
+
+	public static void main(String[] args) {
+		
+		Alumno al = new Alumno("pablo", 5);
+		System.out.println(al.toString());
+	}
+
+}
